fb53888eb5f9a59822362e6270fbe571fabac5dd
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-21 17:51:23 +0000

    refactors setGridContent function

--- src/helpers.js
+++ src/helpers.js
@@ -5,16 +5,10 @@ setHeaderContent = function(heading, subheading, blockNumber) {
   subheading.innerHTML = pageContent.blocks[blockNumber].subheading;
 }
 
-createGridHtml = function(blockNumber) {
-  var gridItems = pageContent.blocks[blockNumber].items;
-  var output = "<div>";
-  gridItems.forEach(function(item) {
-    output += "<div class='grid-item'><a href='" + item.url.gben + "'><img src='./images/" + item.image + "'></a><p>" + item.text + "</p></div>";
-  })
-  return output + "</div>"
-}
-
 setGridContent = function(elementId, blockNumber) {
   targetElement = document.getElementById(elementId);
-  targetElement.innerHTML = createGridHtml(blockNumber);
+  var gridItems = pageContent.blocks[blockNumber].items;
+  gridItems.forEach(function(item) {
+    targetElement.innerHTML += "<div class='grid-item'><a href='" + item.url.gben + "'><img src='./images/" + item.image + "'></a><p>" + item.text + "</p></div>";
+  })
 }
