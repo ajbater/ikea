ccf39209e5171284e119d01f35235ab07e9435a3
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-23 14:28:08 +0000

    uses jquery to make two frame animation

--- src/sleepTips.js
+++ src/sleepTips.js
@@ -1,21 +1,6 @@
-// var sleepTipImagesContainer = document.getElementsByClassName("sleep-tip-images");
-// var firstFrame = document.getElementsByClassName("first-frame");
-// var secondFrame = document.getElementsByClassName("second-frame");
-
-// var timer = setInterval(showSecondFrame, 1000);
-
-// function showSecondFrame() {
-//   for (i=0; i < secondFrame.length; i++) {
-//     if (secondFrame[i].style.visibilty == "hidden") {
-//       secondFrame[i].style.visibility = "visible";
-//     } else {
-//       secondFrame[i].style.visibility = "hidden";
-//     }
-//   }
-// }
-
-// showSecondFrame = function() {
-//   for (i=0; i < secondFrame.length; i++) {
-//     secondFrame[i].style.visibility = "hidden";
-//   }
-// }
+$(function() {
+	var $blinkImage = $(".second-frame");
+	setInterval(function() {
+		$blinkImage.toggleClass("blink");
+	}, 1000);
+});
