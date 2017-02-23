e4b843a6806e35b851174e029f2fee2c159d5a6f
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-23 16:44:38 +0000

    creates basic automatic slideshow

--- src/index.js
+++ src/index.js
@@ -18,6 +18,5 @@ productHeading.innerHTML = pageContent.blocks[4].heading;
 setGridContent('product-grid-items', 4);
 
 // sets content of sleep coach block
-// showSecondFrame();
 setHeaderContent('sleep-coach-heading', 'sleep-coach-subheading', 5);
 setSlidesContent('sleep-coach-carousel-container', 6);
--- src/sleepTips.js
+++ src/sleepTips.js
@@ -4,3 +4,13 @@ $(function() {
 		$blinkImage.toggleClass("blink");
 	}, 1000);
 });
+
+$(function(){
+    $("#sleep-coach-carousel-container .slide:gt(0)").hide();
+    setInterval(function(){
+      $("#sleep-coach-carousel-container > :first-child").fadeOut(1000)
+         .next(".slide").fadeIn(1000)
+         .end()
+         .appendTo("#sleep-coach-carousel-container");
+       }, 3000);
+});
