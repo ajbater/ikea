5820493500fa14b89f6fd87ad01cca08117a9d15
Amanda Bater <amandabater@amandas-mbp.lan>
2017-03-06 13:13:36 +0000

    slight adjustment to fading play button and poster

--- src/youtube.js
+++ src/youtube.js
@@ -3,8 +3,8 @@ $(function(){
   var videoPoster = $("#video-poster");
 	var video = $("#youtube-video");
 	playButton.click(function(ev){
-    playButton.fadeOut(700);
-    videoPoster.fadeOut(700);
+    playButton.fadeOut(1000);
+    videoPoster.fadeOut(1000);
 		video[0].src += "?autoplay=1";
 		ev.preventDefault();
   });
