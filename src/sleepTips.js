9c7baaae9c8f1c51915fffcc7bb33cf5231dddbc
Amanda Bater <amandabater@amandas-mbp.lan>
2017-03-06 13:23:58 +0000

    enables previous button on image carousel

--- src/sleepTips.js
+++ src/sleepTips.js
@@ -7,16 +7,14 @@ $(function() {
 
 $(function(){
     $("#slide-container .slide:gt(0)").hide();
-    $("#next-arrow").click(function(){
+    $("#next-arrow").on('click', function(){
       $("#slide-container > :first-child").slideUp(300, "swing")
          .next(".slide").slideDown(300, "swing")
          .end()
          .appendTo("#slide-container");
        });
-		// $("#prev-arrow").click(function(){
-		// 	$("#slide-container :last-child").slideDown(300, "swing")
-		// 		.prev(".slide").slideDown(300, "swing")
-		// 		.end()
-		// 		.insertBefore($("#slide-container :first-child").slideUp(300, "swing"));
-		// });
+		$("#prev-arrow").on('click', function(){
+			$("#slide-container > :last-child").slideDown(300, "swing")
+				.insertBefore($("#slide-container > :first-child").slideUp(300, "swing"));
+		});
 });
